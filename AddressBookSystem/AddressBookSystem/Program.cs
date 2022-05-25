61af8b861dcf4df438cd0e3d172ac30f293020ac
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-25 19:39:13 +0530

    [Harini]: Displaying details of creation

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using AddressBookSystem;
+
+internal class program
+{
+
+    
+
+    public static void Main(string[] args)
+    {
+        AddressBookSystem.person ab = new AddressBookSystem.person();
+        ab.createcontacts();
+        ab.displaycontacts();
+
+    }
+}
---
+++ AddressBookSystem/AddressBookSystem/person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class person
+    {
+        public List<contacts> people = new List<contacts>();
+        public  void createcontacts()
+        {
+            contacts contact = new contacts();
+            Console.WriteLine("Enter first name: ");
+            contact.firstname = Console.ReadLine();
+
+            Console.WriteLine("Enter lastname name: ");
+            contact.lastname = Console.ReadLine();
+
+            Console.WriteLine("Enter address: ");
+            contact.address = Console.ReadLine();
+
+            Console.WriteLine("Enter contact Number: ");
+            contact.address = Console.ReadLine();
+
+            Console.WriteLine("Enter city: ");
+            contact.city = Console.ReadLine();
+
+
+            Console.WriteLine("Enter zip code: ");
+            contact.zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter state: ");
+            contact.state = Console.ReadLine();
+
+            Console.WriteLine("Enter email: ");
+            contact.email = Console.ReadLine();
+
+            people.Add(contact);
+        }
+        public  void displaycontacts()
+        {
+            if (people.Count == 0)
+            {
+                Console.WriteLine("address book is empty.");
+                return;
+            }
+            Console.WriteLine("list of contacts:\n");
+            foreach (var contact in people)
+            {
+                Console.WriteLine("first name: " + contact.firstname);
+                Console.WriteLine("last name: " + contact.lastname);
+                Console.WriteLine("address: " + contact.address);
+                Console.WriteLine("city: " + contact.city);
+                Console.WriteLine("state: " + contact.state);
+                Console.WriteLine("zip Code: " + contact.zip);
+                Console.WriteLine("contact No: " + contact.phoneNo);
+                Console.WriteLine("email address: " + contact.email);
+            }
+        }
+
+    }
+}
